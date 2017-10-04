8f241f9345bff1a3ea99d95e303dccd95db9dacb
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-04 12:58:02 -0700

    Added add-mult and add-cube

--- arithmetic.py
+++ arithmetic.py
@@ -20,24 +20,29 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
-    return(float(num1)/ num2)
+    return (float(num1) / num2)
 
 def square(num1):
     """Return the square of the input."""
 
-    return(float(num1)**2)
+    return (float(num1) ** 2)
 
 def cube(num1):
     """Return the cube of the input."""
-
-    return(float(num1)**3)
+ 
+    return (float(num1) ** 3)
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
-    return(float(num1)**num2)
+    return (float(num1) ** num2)
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
-   
-    return(float(num1)% num2)
+    return (float(num1) % num2)
+
+def add_mult(num1, num2, num3):
+    return (int((num1+num2)*num3))
+
+def add_cubes(num1, num2):
+    return int((num1**3 + num2**3))
